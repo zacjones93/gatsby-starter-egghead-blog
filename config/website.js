f447008885ad83266b8c4aa3111098efa88f4376
Zac Jones <zacjones93@gmail.com>
2019-02-10 12:36:36 -0500

    updated with personal information and new blog post

--- config/website.js
+++ config/website.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
-  siteTitle: 'My Blog', // Navigation and Site Title
+  siteTitle: 'Zac\'s Blog', // Navigation and Site Title
   siteTitleAlt: 'The blog of an egghead creator', // Alternative Site title for SEO
-  siteTitleShort: 'Starter Blog', // short_name for manifest
-  siteUrl: 'https://your-site.io', // Domain of your site. No trailing slash!
+  siteTitleShort: 'Zacs Blog', // short_name for manifest
+  siteUrl: 'https://zacjones.io', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'This is where they post things!',
-  author: 'Author', // Author for schemaORGJSONLD
-  organization: 'egghead.io LLC',
+  siteLogo: 'images/flowers.jpg', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'The various interests documented by Zac',
+  author: 'Zac Jones', // Author for schemaORGJSONLD
+  organization: 'egghead.io ',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
+  userTwitter: '@zacjones93', // Twitter Username
   ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: '',
@@ -21,8 +21,8 @@ module.exports = {
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
+  twitter: 'https://twitter.com/zacjones93/',
+  twitterHandle: '@zacjones93',
+  github: 'https://github.com/zacjones93/',
   linkedin: '',
 }
